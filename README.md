ecd778ad689b2939514b88e93ac185928cd77f8b
Guilherme Gabriel <guilhermag@gmail.com>
2023-06-13 18:19:02 -0300

    finalizacao projeto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Desafio de projeto kotlin
+
+Desafio de Projeto criado para avaliação do conteúdo técnico explorado no repositório aprenda-kotlin-com-exemplos. Nesse
+contexto, iremos abstrair o seguinte domínio de aplicação:
+
+## Regras de negócio
+
+A DIO possui Formacoes incríveis que visam oferecer um conjunto de ConteudosEducacionais voltados para uma
+stack tecnológica específica, preparando profissionais de TI para o mercado de trabalho. Formacoes possuem algumas
+características importantes, como nome, nivel e seus respectivos conteudosEducacionais. Além disso, tais experiências
+educacionais têm um comportamento relevante ao nosso domínio, definido pela capacidade de matricular um ou mais Alunos.
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,47 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+
+class Usuario (val name: String)
+
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
+
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(usuario: Usuario) {
+        inscritos.add(usuario)
+    }
+
+    override fun toString(): String {
+        return "Formacao(nome='$nome', conteudos=$conteudos, nivel=$nivel, inscritos=$inscritos)"
+    }
+
+
+}
+
+fun main(args: Array<String>) {
+    val angular = listOf(
+        ConteudoEducacional("introdução ao angular", 100),
+        ConteudoEducacional("componentes", 150),
+        ConteudoEducacional("projetos avançados")
+    )
+
+    val react = listOf(
+        ConteudoEducacional("introdução ao react", 80),
+        ConteudoEducacional("hooks", 100),
+        ConteudoEducacional("projetos avançados")
+    )
+
+    val paulo = Usuario("Guilherme Gabriel")
+    val felipe = Usuario("Miško Hevery")
+    val daniela = Usuario("Jordan Walke")
+
+    val formacaoAngular = Formacao("Angular", angular, Nivel.INTERMEDIARIO)
+    val formacaoReact = Formacao("React", react, Nivel.BASICO)
+
+
+    println(formacaoAngular)
+    println(formacaoReact)
 }
